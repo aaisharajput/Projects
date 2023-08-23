2ac822476b5ce5ea9b365349bd76cc81cd5e881a
aaisharajput <109970085+aaisharajput@users.noreply.github.com>
2023-08-23 14:25:29 +0530

    Add files via upload

---
+++ weekend/js/main.js
@@ -0,0 +1,27 @@
+
+
+  $(".carousel").owlCarousel({
+           margin: 35,
+           loop: true,
+           autoplay: true,
+           autoplayTimeout: 1000,
+           autoplayHoverPause: true,
+           responsive: {
+             0:{
+               items:1,
+               nav: false
+             },
+             200:{
+               items:2,
+               nav: false
+             },
+             600:{
+               items:3,
+               nav: false
+             },
+             1000:{
+               items:4,
+               nav: false
+             }
+           }
+         });
